c3e95488e9d0ecc55e45bda3561519b2a8283eef
Robert Mara <robertmarava@gmail.com>
2019-07-07 18:29:27 -0400

    Added has_many associations, so tests pass.  I do not know if the code works, but the tests pass.

--- app/models/sayer.rb
+++ app/models/sayer.rb
@@ -1,2 +1,7 @@
 class Sayer < ApplicationRecord
+
+  has_many :sayings, dependent: :destroy
+  has_many :sayer_sources, dependent: :destroy
+  has_many :sources, through: :sayer_sources
+
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,2 +1,6 @@
 class Source < ApplicationRecord
+
+  has_many :sayer_sources, dependent: :destroy
+  has_many :sayers, through: :sayer_sources
+
 end
